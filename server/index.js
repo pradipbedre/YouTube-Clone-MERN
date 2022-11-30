23ea380360b1d196ee54755bd9700d9f0f186612
Pradip Bedre <bedrepradip0@gmail.com>
2022-12-01 00:30:55 +0530

    mongo db atlas and express connection compleate

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 2000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+}
+main()
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
