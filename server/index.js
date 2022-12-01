a9e2e9195caa294ac30223a0671c9ada8a17361d
Pradip Bedre <bedrepradip0@gmail.com>
2022-12-01 20:50:48 +0530

    specific reason for error handling feature compleated

--- server/index.js
+++ server/index.js
@@ -37,6 +37,19 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videosRoutes);
 app.use("/api/comments", commentRoutes);
+
+
+// Specific Error handing using middleware 
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+})
+
 // Server Listening
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
